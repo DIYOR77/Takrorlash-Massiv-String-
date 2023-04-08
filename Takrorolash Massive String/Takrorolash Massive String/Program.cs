296f9eb986c1aba4c5632dcc502f3b21c6e7a44a
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-08 13:16:23 +0500

    Takrorlash

---
+++ Takrorolash Massive String/Takrorolash Massive String/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Takrorolash_Massive_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
